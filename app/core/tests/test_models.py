715cbe78f1dc658f6cd2c0dff63e68d16a7362d1
dev-tanvir <tanvirfaisaldev@gmail.com>
2021-07-03 15:33:41 +0600

    testing ssh connection

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -17,4 +17,4 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(user.email, email)
-        self.assertTrue(user.check_password(password))
+        self.assertTrue(user.check_password(password)) # comes with django user model - as password is encrypted 
