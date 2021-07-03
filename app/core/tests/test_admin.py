d20b8155c104cd3c78695061c407591bb3b2b0be
dev-tanvir <tanvirfaisaldev@gmail.com>
2021-07-04 02:13:18 +0600

    added unit test for listed users for core app

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,29 @@
+from django.http import response
+from django.test import TestCase, Client
+from django.urls import reverse
+from django.contrib.auth import get_user_model
+
+
+class AdminSiteTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.admin_user = get_user_model().objects.create_superuser(
+            email='admin@admin.com',
+            password='admin123'
+        )
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email='nuser@g.com',
+            password='user123',
+            name='Tanvir Faisal'
+        )
+
+    def test_users_listed(self):
+        """Testing custom users listing"""
+        url = reverse("admin:core_users_changelist")
+        response = self.client.get(url)
+
+        self.assertContains(response, self.user.name)
+        self.assertContains(response, self.user.email)
+
