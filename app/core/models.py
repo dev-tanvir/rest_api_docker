c4022ade6c3a5555086d1da9d6e658e16cdec24b
dev-tanvir <tanvirfaisaldev@gmail.com>
2021-07-04 01:06:03 +0600

    updated with new tests,initial migration and custom user model

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
+                                        PermissionsMixin
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password=None, **extra_fields):
+        """ Creates and saves a new user"""
+        if not email:
+            raise ValueError('Users need to pass an valid email address!')
+
+        user = self.model(email=self.normalize_email(email), **extra_fields) # not using password here cause
+                                                        # password needs to be hashed and 
+                                                        # not saved in clear text
+
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Custom User model that supports Creating a user using email in stead of username"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = "email"
+
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -6,15 +6,25 @@ class ModelTests(TestCase):
     """ Testing user creation"""
 
     def test_user_creation_email_successful(self):
-        """ Testing user creation """
+        """ Testing user creation with email"""
 
         email = "tanvirfaisaldev@gmail.com"
         password = "123456"
 
-        user = get_user_model().objects.create_user(
-            email=email,
-            password=password
-        )
+        user = get_user_model().objects.create_user(email=email, password=password)
 
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password)) # comes with django user model - as password is encrypted 
+
+    def test_new_user_email_normalization(self):
+        """Testing new user email normalization"""
+
+        email = "tanvirfaisaldev@GMAIL.COM"
+
+        user = get_user_model().objects.create_user(email, 'test123')
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_email_validated(self):
+        with self.assertRaises(ValueError): # it tests if following condition raises valueerror, if not test fails
+            get_user_model().objects.create_user(None, 'test123')
