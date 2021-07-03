479e752cac7d5f507eebf235fe95974480d06baa
dev-tanvir <tanvirfaisaldev@gmail.com>
2021-07-04 03:47:03 +0600

    added unit test for admin site new user add page +  admin site add_fieldsets for add page

--- app/core/tests/test_admin.py
+++ app/core/tests/test_admin.py
@@ -10,13 +10,13 @@ class AdminSiteTests(TestCase):
         self.client = Client()
         self.admin_user = get_user_model().objects.create_superuser(
             email='admin@admin.com',
-            password='admin123'
+            password='admin123',
         )
         self.client.force_login(self.admin_user)
         self.user = get_user_model().objects.create_user(
             email='nuser@g.com',
             password='user123',
-            name='Tanvir Faisal'
+            name='Tanvir Faisal',
         )
 
     def test_users_listed(self):
@@ -27,9 +27,16 @@ class AdminSiteTests(TestCase):
         self.assertContains(response, self.user.name)
         self.assertContains(response, self.user.email)
 
-    def test_users_change_page(self):
+    def test_users_edit_page(self):
         """Test the user edit page works"""
         url = reverse("admin:core_user_change", args=[self.user.id])    # /admin/core/user/1
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_new_users_add(self):
+        """Test the new user create page works"""
+        url = reverse("admin:core_user_add")
         response = self.client.get(url)
 
         self.assertEqual(response.status_code, 200)
