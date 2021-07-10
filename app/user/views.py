a6fe3958590caf312ba260769558b065face117c
dev-tanvir <tanvirfaisaldev@gmail.com>
2021-07-10 08:07:45 +0600

    added retrieve api + generics.RetrieveUpdateAPIView  + urls

--- app/user/views.py
+++ app/user/views.py
@@ -1,14 +1,30 @@
-from rest_framework import generics
+from rest_framework import generics, authentication, permissions
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 from user.serializers import UserSerializer, AuthTokenSerializer
 
 class CreateUserView(generics.CreateAPIView):
     """Creates a new user in the system"""
+    
     serializer_class = UserSerializer
 
 
 class CreateTokenView(ObtainAuthToken):
     """Creates a new auth token for the requested user"""
+    
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Mange the authenticated users"""
+
+    serializer_class = UserSerializer
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self):
+        """Retrieve and return authenticated user"""
+        return self.request.user # request will have user due to authentication class. thanks to DRF, same is out-of-box for django
+
+
