32a7e470c81b06ae9dc66419beb6c502b5a2d3fe
dev-tanvir <tanvirfaisaldev@gmail.com>
2021-07-12 07:20:31 +0600

    Refactored to apply DRY principle for Synthesize viewsets

--- app/synthesize/views.py
+++ app/synthesize/views.py
@@ -5,37 +5,29 @@ from rest_framework.permissions import IsAuthenticated
 from core.models import Tag,Chemcomp
 from synthesize import serializers
 
-class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, 
+
+class SynthesizeElementViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, 
                 mixins.CreateModelMixin):
-    """Manage Tags in the database"""
-    serializer_class = serializers.TagSerializer
+    """Manage Synthesize elements in the database"""
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
-    queryset = Tag.objects.all()
 
     def get_queryset(self):                                         # filter per user
-        """Extending it show only logged user owned tags"""
-
+        """Extending it show only logged user owned Synthesize elements"""
         return self.queryset.filter(user=self.request.user).order_by('-name')
 
     def perform_create(self, serializer):
-        """Create a tag"""
-        serializer.save(user=self.request.user)     # This is to tag logged in user as tag user
+        """Create a Synthesize elements"""
+        serializer.save(user=self.request.user)     # This is to set logged in user as element user
 
 
-class ChemcompViewSet(viewsets.GenericViewSet, mixins.ListModelMixin,
-                    mixins.CreateModelMixin):
+class TagViewSet(SynthesizeElementViewSet):
+    """Manage Tags in the database"""
+    serializer_class = serializers.TagSerializer
+    queryset = Tag.objects.all()
+
+
+class ChemcompViewSet(SynthesizeElementViewSet):
     """Manage Chemcomps in the database"""
     serializer_class = serializers.ChemcompSerializer
-    permission_classes = (IsAuthenticated,)
-    authentication_classes = (TokenAuthentication,)
     queryset = Chemcomp.objects.all()
-
-    def get_queryset(self):                                        
-        """Extending it show only logged user owned chemcomps"""
-
-        return self.queryset.filter(user=self.request.user).order_by('-name')
-
-    def perform_create(self, serializer):
-        """Create a chemcomp"""
-        serializer.save(user=self.request.user)
